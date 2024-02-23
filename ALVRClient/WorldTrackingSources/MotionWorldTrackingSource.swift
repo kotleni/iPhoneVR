17844b695cf3bb83bdc2004c914d91fb09fcd16c
kotleni <yavarenikya@gmail.com>
2024-02-23 22:17:32 +0200

    refactor: motion world tracking source

--- ALVRClient/WorldTrackingSources/MotionWorldTrackingSource.swift
+++ ALVRClient/WorldTrackingSources/MotionWorldTrackingSource.swift
@@ -9,36 +9,46 @@ import CoreMotion
 
 // FIXME: Not working yet!
 class MotionWorldTrackingSource: NSObject, WorldTrackingSource {
-    private let dispatchQueue: DispatchQueue
+    // private let dispatchQueue: DispatchQueue
+    private let operationQueue: OperationQueue
     private let motionManager: CMMotionManager
     
-    private var lastTickTime: Int64 = 0
-    private var tps = 0
-    
-    // FIXME: Monkey code
-    private var linearVelocity: (Float, Float, Float) = (Float.zero, Float.zero, Float.zero)
-    private var position: (Float, Float, Float) = (Float.zero, Float.zero, Float.zero)
-    private var rotation: (Float, Float, Float) = (Float.zero, Float.zero, Float.zero)
+    private var linearVelocity: (Float, Float, Float) = (0.0, 0.0, 0.0)
+    private var position: (Float, Float, Float) = (0.0, 0.0, 0.0)
+    private var rotation: (Float, Float, Float) = (0.0, 0.0, 0.0)
     
     override init() {
-        dispatchQueue = .init(label: "ARWorldTrackingSource", qos: .background)
+        // dispatchQueue = DispatchQueue(label: "MotionWorldTrackingSource DispatchQueue", qos: .background)
+        operationQueue = .init()
+        operationQueue.name = "MotionWorldTrackingSource OperationQueue"
+        operationQueue.qualityOfService = .background
         
-        motionManager = .init()
-        motionManager.deviceMotionUpdateInterval = 1000 / 30 // 30 tps
+        motionManager = CMMotionManager()
+        motionManager.deviceMotionUpdateInterval = 1.0 / 30.0 // 30 tps
         
         super.init()
     }
     
     func start() {
-        motionManager.startDeviceMotionUpdates(to: .main) { [weak self] motion, error in
-            if error != nil { return }
-            guard let self = self else { return }
-            guard let motion = motion else { return }
+        guard motionManager.isDeviceMotionAvailable else {
+            // TODO: Handle the case where device motion is not available
+            print("Device motion is not available.")
+            return
+        }
+    
+        motionManager.startDeviceMotionUpdates(to: operationQueue) { [weak self] (motion, error) in
+            guard let self = self, let motion = motion else {
+                return
+            }
             
-            let attitude = motion.attitude
-            rotation = (Float(attitude.roll), Float(attitude.pitch), Float(attitude.yaw))
+            // FIXME: Not working
+            // self.linearVelocity = (Float(motion.gravity.y), Float(motion.userAcceleration.z), Float(motion.userAcceleration.z))
+            self.position = (0, 1.1, 0)
+            //self.rotation = (Float(motion.rotationRate.x), Float(motion.rotationRate.y), Float(motion.rotationRate.z))
             
-            position.2 = 1.7
+            self.rotation.0 = Float(motion.userAcceleration.x)
+            self.rotation.1 = Float(motion.userAcceleration.y)
+            self.rotation.2 = Float(motion.userAcceleration.z)
         }
     }
     
